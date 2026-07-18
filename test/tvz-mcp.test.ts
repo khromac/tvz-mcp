645f1ded0514eb85444f97d796184ed4c0160051
khromac <25547789+khromac@users.noreply.github.com>
2026-07-18 16:23:50 +0200

    chore: initialize project for aws

---
+++ test/tvz-mcp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TvzMcp from '../lib/tvz-mcp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tvz-mcp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TvzMcp.TvzMcpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
