f2e1b3e81eb9cd1b40bc3156c72429ab210b178c
msmrcek <25547789+khromac@users.noreply.github.com>
2026-07-18 16:58:43 +0200

    Chore: aws init (#1)
    * chore: initialize project for aws
    
    * test: verify signed commit
    
    * test: webstorm personal

---
+++ bin/tvz-mcp.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib/core';
+import { TvzMcpStack } from '../lib/tvz-mcp-stack';
+
+const app = new cdk.App();
+
+new TvzMcpStack(app, 'TvzMcpStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION ?? 'eu-central-1',
+    },
+});
